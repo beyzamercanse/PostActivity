af84de776963730fb7e348a62663afabe2e89e3a
beyzamercan97 <beyzamercan@yonsei.ac.kr>
2021-11-28 22:31:47 +0300

    utils 추가히기

---
+++ app/src/main/java/com/example/post/Utils.java
@@ -0,0 +1,24 @@
+package com.example.post;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public class Utils {
+    public static Bitmap loadBitmap(Context context, Uri uri) {
+        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+        Cursor cursor = context.getContentResolver().query(uri,
+                filePathColumn, null, null, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+
+        return BitmapFactory.decodeFile(picturePath);
+    }
+}
